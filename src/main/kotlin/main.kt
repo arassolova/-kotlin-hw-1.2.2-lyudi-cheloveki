4c58c0dd79c014c188b91d895dc8d6a41f1c8ba2
Anastasiya Rassolova <avmurueva@gmail.com>
2020-09-21 09:23:13 +0300

    check for 11 added, checking method extracted

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,24 @@
 fun main() {
-    val likes = 1002
-    val result = likes % 10
-    if (result === 1) {
-        println("Понравилось $likes человеку")
+    val likes1 = 1
+    val likes2 = 11
+    val likes3 = 111
+    val likes4 = 1011
+    val likes5 = 1001
+    val likes6 = 458
+    printLikes(likes1)
+    printLikes(likes2)
+    printLikes(likes3)
+    printLikes(likes4)
+    printLikes(likes5)
+    printLikes(likes6)
+}
+
+private fun printLikes(likes: Int) {
+    val result10 = likes % 10
+    val result100 = likes % 100
+    if (result10 == 1) {
+        if (result100 == 11) println("Понравилось $likes людям")
+        else println("Понравилось $likes человеку")
     } else {
         println("Понравилось $likes людям")
     }
